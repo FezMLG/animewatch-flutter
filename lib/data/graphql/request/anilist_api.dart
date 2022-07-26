ec8c8f562c35924aeef66a9b53853a399e600938
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-07-26 18:22:30 +0200

    fixing incorrect type error

--- lib/data/graphql/request/anilist_api.dart
+++ lib/data/graphql/request/anilist_api.dart
@@ -1,12 +1,12 @@
 import 'package:animewatch/data/graphql/api_client.dart';
 import 'package:animewatch/data/graphql/query.dart';
-import 'package:animewatch/services/models/list_of_series_model.dart';
+import 'package:animewatch/services/models/page_of_series_model.dart';
 
 class AniList extends GraphQLAPIClient {
-  Future<List<Media>> getListOfSeries({required int id}) async {
+  Future<List<SeriesCard>> getListOfSeries({required int id}) async {
     final result = await execute(listOfAnimeQuery);
-    var snapshot = result.data!["Page"]["media"];
-    var listOfSeries = snapshot.map((d) => Media.fromJson(d));
-    return listOfSeries.toList();
+    List snapshot = result.data!["Page"]["media"];
+    var listOfSeries = snapshot.map((d) => SeriesCard.fromJson(d)).toList();
+    return listOfSeries;
   }
 }
